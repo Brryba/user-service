95621c934e39d4199ecc1b0eb24bd0b370a52aed
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-07-15 17:59:34 +0300

    pull request issues solved

--- src/main/java/user_service/dto/user/UserRequestDto.java
+++ src/main/java/user_service/dto/user/UserRequestDto.java
@@ -18,14 +18,14 @@ import java.time.LocalDate;
 public class UserRequestDto {
     @NotBlank(message = "Name is required")
     @Size(max = 100, message = "Name must not be longer than 100 letters")
-    protected String name;
+    private String name;
     @NotBlank(message = "Surname is required")
     @Size(max = 100, message = "Surname must not be longer than 100 letters")
-    protected String surname;
+    private String surname;
     @Past(message = "Birth date must be in the past")
-    protected LocalDate birthDate;
+    private LocalDate birthDate;
     @NotBlank(message = "Email is required")
     @Size(max = 100, message = "Email must be shorter than 100 symbols")
     @Email(message = "Incorrect email format")
-    protected String email;
+    private String email;
 }
