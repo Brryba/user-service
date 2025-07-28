8a3b1fc6c825f6a525bfd5af8fc744df6214983a
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-07-28 12:26:56 +0300

    unused methods removed

--- src/main/java/user_service/controller/UserController.java
+++ src/main/java/user_service/controller/UserController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,10 +28,6 @@ public class UserController {
     }
 
     private final UserService userService;
-
-    private long getAuthenticatedUserId() {
-        return (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
 
     @GetMapping("/me")
     @ResponseStatus(HttpStatus.OK)
@@ -59,12 +54,6 @@ public class UserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCurrentUser(@AuthenticationPrincipal Long userId) {
         userService.deleteUser(userId);
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserResponseDto addUser(@RequestBody @Valid UserRequestDto user) {
-        return userService.createUser(user);
     }
 
     @GetMapping("/{id}")
